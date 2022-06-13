e63d8c371d41070df3e7d8c450ea4c9362b631df
dariaKretiuk <kretyukdasha@gmail.com>
2022-06-13 15:05:15 +0700

    Add project

---
+++ ProjectCoreData/Sources/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectCoreData
+//
+//  Created by Дарья Кретюк on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
